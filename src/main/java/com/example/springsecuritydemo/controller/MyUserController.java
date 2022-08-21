c103ee7eaad3c66eee8ed8c5bb7283ecd438e1f7
Onitsiky <hei.onitsiky@gmail.com>
2022-08-21 23:04:06 +0300

    add endpoint to create user

--- src/main/java/com/example/springsecuritydemo/controller/MyUserController.java
+++ src/main/java/com/example/springsecuritydemo/controller/MyUserController.java
@@ -1,9 +1,23 @@
 package com.example.springsecuritydemo.controller;
 
+import com.example.springsecuritydemo.model.MyUser;
+import com.example.springsecuritydemo.repository.MyUserRepository;
+import com.example.springsecuritydemo.service.MyUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyUserController {
 
+    @Autowired
+    private MyUserRepository userRepository;
+    @Autowired
+    private MyUserService userService ;
+
+    @PostMapping("/authorized")
+    public String addUser(@RequestBody MyUser user){
+        return userService.createUser(user);
+    }
 }
--- src/main/java/com/example/springsecuritydemo/service/MyUserService.java
+++ src/main/java/com/example/springsecuritydemo/service/MyUserService.java
@@ -2,15 +2,19 @@ package com.example.springsecuritydemo.service;
 
 import com.example.springsecuritydemo.model.MyUser;
 import com.example.springsecuritydemo.repository.MyUserRepository;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@AllArgsConstructor
 public class MyUserService implements UserDetailsService {
     @Autowired
     private MyUserRepository userRepository;
@@ -21,5 +25,15 @@ public class MyUserService implements UserDetailsService {
             throw new UsernameNotFoundException("User not found");
         }
         return new MyUserDetails(user);
+    }
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    public String createUser(MyUser user){
+        String encryptPasswd = getPasswordEncoder().encode(user.getPassword());
+        user.setPassword(encryptPasswd);
+        userRepository.save(user);
+        return "User Saved !";
+
     }
 }
